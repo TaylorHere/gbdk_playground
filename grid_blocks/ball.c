90393017a3167db3fc3c8fef65ba3ca15b47f845
李彦宏 <liyanhong@hz-health.cn>
2020-02-05 06:00:19 +0800

    feat:

--- grid_blocks/ball.c
+++ grid_blocks/ball.c
@@ -4,32 +4,81 @@
 #include <gb/drawing.h>
 
 typedef struct {
-    int x, y;
+    int x, y, min_x, min_y, max_x, max_y;
 } Ball;
 
-void Ball_new(Ball *const me, int x, int y) {
+void Ball_new(Ball *const me, int x, int y, int min_x, int min_y, int max_x, int max_y) {
     me->y = y;
     me->x = x;
+    me->min_x = min_x;
+    me->min_y = min_y;
+    me->max_x = max_x;
+    me->max_y = max_y;
+}
+
+
+int Ball_get_y(Ball *const me){
+    if (me->y <= me->min_y){
+        me->y = me->min_y;
+    } else if (me->y >= me->max_y){
+        me->y = me->max_y;
+    }
+    return me->y;
+}
+
+int Ball_get_x(Ball *const me){
+    if (me->x <= me->min_x){
+        me->x = me->min_x;
+    } else if (me->x >= me->max_x){
+        me->x = me->max_x;
+    }
+    return me->x;
+}
+
+
+void Ball_set_y(Ball *const me, int y){
+    if (y <= me->min_y){
+        me->y = me->min_y;
+    } else if (me->y >= me->max_y){
+        me->y = me->max_y;
+    } else {
+        me->y = y;
+    }
+}
+
+void Ball_set_x(Ball *const me, int x){
+    if (x <= me->min_x){
+        me->x = me->min_x;
+    } else if (me->x >= me->max_x){
+        me->x = me->max_x;
+    } else {
+        me->x = x;
+    }
 }
 
 void Ball_left_move(Ball *const me, int steps){
     me->x -= steps;
+    Ball_set_x(me, me->x);
 }
-
 
 void Ball_up_move(Ball *const me, int steps){
     me->y -= steps;
+    Ball_set_y(me, me->y);
 }
 
 void Ball_right_move(Ball *const me, int steps){
     me->x += steps;
+    Ball_set_x(me, me->x);
 }
 
 void Ball_down_move(Ball *const me, int steps){
     me->y += steps;
+    Ball_set_y(me, me->y);
 }
+
 
 void Ball_del(Ball *const me){
     free(me);
 }
+
 #endif
--- grid_blocks/board.c
+++ grid_blocks/board.c
@@ -15,18 +15,19 @@ typedef struct Board{
 
 void Board_new(Board *const me, int grid_rows, int grid_cols, int grid_size, int ball_x, int ball_y){
     Grid_new(&me->grid, grid_rows, grid_cols, grid_size);
-    Ball_new(&me->ball, ball_x, ball_y);
+    Ball_new(&me->ball, ball_x, ball_y, 0, 0, grid_rows, grid_cols);
 }
 
 int Board_get_ball_radius(Board *const me){
     return (int)(me->grid.size / 2);
 }
-int Board_get_ball_real_x(Board *const me){
-    return (me->ball.x * me->grid.size) + Board_get_ball_radius(me)+ me->init_x;
-}
 
 int Board_get_ball_real_y(Board *const me){
-    return (me->ball.y * me->grid.size) + Board_get_ball_radius(me)+ me->init_y;
+    return (Ball_get_y(&me->ball) * me->grid.size) + Board_get_ball_radius(me) + me->init_y;
+}
+
+int Board_get_ball_real_x(Board *const me){
+    return (Ball_get_x(&me->ball) * me->grid.size) + Board_get_ball_radius(me) + me->init_x;
 }
 
 void Board_del(Board *const me){
